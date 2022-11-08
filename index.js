804578d2f52cf35037b4d17fbd0303805fc236dd
parvezahmedweb1 <parvezahmedweb1@gmail.com>
2022-11-08 12:06:34 +0600

    server is connected

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("colors");
+// ? middle ware
+app.use(cors());
+app.use(express.json());
+// ------------------------------------
+app.get("/", (req, res) => {
+  res.send("GetHost Server is Running...");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`.bgCyan);
+});
