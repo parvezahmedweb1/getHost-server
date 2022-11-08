c33edccee2f155ba6dc6d37ec2d748440bf6ac9d
parvezahmedweb1 <parvezahmedweb1@gmail.com>
2022-11-08 12:22:52 +0600

    database is connected

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("colors");
+require("dotenv").config();
 // ? middle ware
 app.use(cors());
 app.use(express.json());
+// ? mongodb
+const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASSWORD}@cluster0.mc1suux.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// ? dbConnected
+const dbConnected = async () => {
+  try {
+    await client.connect();
+    console.log("Database is Connected".yellow);
+  } catch (error) {
+    console.log(error.name, error.message, error.state);
+  }
+};
+dbConnected();
 // ------------------------------------
 app.get("/", (req, res) => {
   res.send("GetHost Server is Running...");
